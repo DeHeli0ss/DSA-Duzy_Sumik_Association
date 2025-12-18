d8083611443ad06e49d1fd2b60228a5aead05f0f
Heli0s <darthhelios44@gmail.com>
2025-12-18 20:50:39 +0100

    Rename table and update column names in SQL

--- TABELA KONTO SYSTEMOWE.sql
+++ TABELA KONTO SYSTEMOWE.sql
@@ -2,22 +2,16 @@
 --  Tworzenie tabeli Konto Systemowe gdzie można    --
 --  logować się za pomocą nr_teleonu lub emaila     --
 ------------------------------------------------------
-CREATE TABLE FP_KONTO_SYSTEMOWE(
-        id_członka NUMERIC(7) PRIMARY KEY,
-        nr_telefonu NUMERIC(9) NULL UNIQUE,
+CREATE TABLE KONTO_SYSTEMOWE(
+        id_czlonka NUMBER(7) PRIMARY KEY,
+        nr_telefonu NUMBER(9) NULL UNIQUE,
         email VARCHAR2(80) NULL UNIQUE,
-        hasło VARCHAR2(20) NOT NULL     
-);
+        haslo VARCHAR2(20) NOT NULL,
 ------------------------------------------------------------------------
 --  Sprawdzenie czy do logowania spełniony jest warunek minimum czyli --
 --  czyli podanie nr_teleonu lub emaila                               --
-------------------------------------------------------------------------
-ALTER TABLE FP_KONTO_SYSTEMOWE
-ADD CONSTRAINT check_logowanie CHECK
-(email is NOT NULL or nr_telefonu is NOT NULL);
-
-ALTER TABLE FP_KONTO_SYSTEMOWE
-RENAME id_członka TO ID_czlonka;
-ALTER TABLE FP_KONTO_SYSTEMOWE
-RENAME COLUMN hasło TO haslo;
+------------------------------------------------------------------------    
+        CONSTRAINT check_logowanie CHECK
+        (email is NOT NULL or nr_telefonu is NOT NULL)
+);
 
