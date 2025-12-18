39e29db545dbe98218cedf8697c29f88c71c750d
Heli0s <darthhelios44@gmail.com>
2025-12-18 21:22:28 +0100

    Rename and modify karta_wedkarska table structure

--- TABELA KARTA WEDKARSKA.sql
+++ TABELA KARTA WEDKARSKA.sql
@@ -1,9 +1,9 @@
-CREATE TABLE fp_karta_wedkarska(
-    Nr_karty NUMERIC(12) NOT NULL UNIQUE,
-    ID_wlasciciela NUMERIC(7) NOT NULL UNIQUE,
+CREATE TABLE karta_wedkarska(
+    Nr_karty NUMBER(12) PRIMARY KEY,
+    ID_wlasciciela NUMBER(7) NOT NULL UNIQUE,
     Typ VARCHAR2(15) NOT NULL,
     DATA_WAZNOSCI DATE NOT NULL,
     CONSTRAINT fk_czlonek_karta_ID_wlasciciela 
     FOREIGN KEY(ID_wlasciciela)
-    REFERENCES fp_członek_klubu(ID_czlonka)
+    REFERENCES czlonek_klubu(ID_czlonka)
 );
