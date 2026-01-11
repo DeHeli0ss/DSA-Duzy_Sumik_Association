8a034c5a7c7e0117f9b33a87e9b411e80d7dc34a
Heli0s <darthhelios44@gmail.com>
2026-01-11 01:17:38 +0100

    Fix table creation syntax in Wyprawa_wedkarska

--- Wyprawa_wędkarska.sql
+++ Wyprawa_wędkarska.sql
@@ -1,9 +1,10 @@
 CREATE TABLE Wyprawa_wedkarska(
     ID_wyprawy NUMBER(12) PRIMARY KEY,
-    ID_miejsca_wyprawy NUMBER(12) NOT NULL UNIQUE,
+    ID_miejsca_wyprawy NUMBER(12) NOT NULL,
     Data_i_godzina_przyjazdu DATE NOT NULL,
     Data_i_godzina_odjazdu DATE NOT NULL,
     CONSTRAINT fk_wyprawa_lowsika_miejsce FOREIGN KEY (ID_miejsca_wyprawy) 
     REFERENCES Lowisko(ID_lowiska)
+
 
 );
