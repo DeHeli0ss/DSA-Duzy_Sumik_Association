4960cb7cd7950f07c26009eb8760c42db8917f7c
Heli0s <darthhelios44@gmail.com>
2026-01-11 01:35:35 +0100

    Fix formatting of Wypozyczony_sprzet table creation

--- Wypożyczony_sprzet.sql
+++ Wypożyczony_sprzet.sql
@@ -5,8 +5,9 @@ CREATE TABLE Wypozyczony_sprzet(
     Poczatek_wypozyczenia DATE NOT NULL,
     Długosc_wypozyczenia NUMBER(3) NOT NULL,
     Kwota_wypozyczenia NUMBER(6,2) NOT NULL,
-    CONSTRAINT fk_wypozczyczony_sklad_sprzet FOREIGN KEY (ID_sprzetu) REFERENCES Sklad_wypozyczalni(ID_sprzetu),
-    CONSTRAINT fk__wypozczyczony_czlonek_osoba_wypozyczajaca FOREIGN KEY (ID_osoby_wypozyczajacej) REFERENCES CZLONEK_KLUBU(ID_czlonka)
+    CONSTRAINT fk_wypozyczony_sklad_sprzet FOREIGN KEY (ID_sprzetu) REFERENCES Sklad_wypozyczalni(ID_sprzetu),
+    CONSTRAINT fk__wypozyczony_czlonek_osoba_wypozyczajaca FOREIGN KEY (ID_osoby_wypozyczajacej) REFERENCES CZLONEK_KLUBU(ID_czlonka)
 
 
 );
+
