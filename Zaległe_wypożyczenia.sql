8e4e47fb38492eaf51c8e82ea574b384f8c81ff6
Heli0s <darthhelios44@gmail.com>
2026-01-11 10:53:21 +0100

    Change ID_wypozyczenia to PRIMARY KEY in table

--- Zaległe_wypożyczenia.sql
+++ Zaległe_wypożyczenia.sql
@@ -1,8 +1,9 @@
 CREATE TABLE Zalegle_wypozyczenia(
-    ID_wypozyczenia NUMBER(12) NOT NULL UNIQUE,
+    ID_wypozyczenia NUMBER(12) PRIMARY KEY,
     Data_poczatku_wypozyczenia DATE NOT NULL,
     Dlugosc_wypozyczenia NUMBER(3) NOT NULL,
     Dlugosc_przetrzymania NUMBER(3) NOT NULL,
     Kwota_zadluzenia NUMBER(7,2) NOT NULL,
     CONSTRAINT fk_zalegle_wypozyczenia_ID_wypozyczenia FOREIGN KEY (ID_wypozyczenia) REFERENCES Wypozyczony_sprzet(ID_wypozczenia)
+
 );
