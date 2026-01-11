d44128aeab48da36c023ec321d1524de9590814a
Heli0s <darthhelios44@gmail.com>
2026-01-11 10:53:41 +0100

    Update Zwrócony_sprzęt.sql

--- Zwrócony_sprzęt.sql
+++ Zwrócony_sprzęt.sql
@@ -1,8 +1,9 @@
 CREATE TABLE Zwrocony_sprzet(
-    ID_wypozczenia NUMBER(12) NOT NULL UNIQUE,
+    ID_wypozczenia NUMBER(12) PRIMARY KEY,
     Data_zwrotu DATE NOT NULL,
     Stan_zwrotu VARCHAR2(20) NOT NULL,
     Kwota_wypozyczenia NUMBER(6,2) NOT NULL,
     CONSTRAINT fk_Zwrocony_wypozyczony FOREIGN KEY (ID_wypozczenia) REFERENCES Wypozyczony_sprzet(ID_wypozczenia)
+
 
 );
