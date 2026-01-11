3f51356bcfe50418708464d4c54889f2e8192965
Heli0s <darthhelios44@gmail.com>
2026-01-11 10:40:02 +0100

    Update Sprzęt_na_wyprawach.sql

--- Sprzęt_na_wyprawach.sql
+++ Sprzęt_na_wyprawach.sql
@@ -1,11 +1,11 @@
 CREATE TABLE SPRZET_UZYTY_NA_WYPRAWACH(
     ID_wyprawy NUMBER(12) PRIMARY KEY,
     ID_sprzetu NUMBER(12) NOT NULL,
-    Id_wlasciciela NUMBER(7) NOT NULL,
+    CONSTRAINT PK_SPRZET_WYPRAWA PRIMARY KEY (ID_wyprawy, ID_sprzetu),
     CONSTRAINT fk_sprzet_na_wyprawach_wyprawa FOREIGN KEY (ID_wyprawy) REFERENCES WYPRAWA_WEDKARSKA(ID_wyprawy),
-    CONSTRAINT fk_sprzet_na_wyprawach_sprzet FOREIGN KEY (ID_sprzetu) REFERENCES SPRZET(ID_sprzetu),
-    CONSTRAINT fk_sprzet_na_wyprawach_wlasciciel FOREIGN KEY (Id_wlasciciela) REFERENCES czlonek_klubu(ID_czlonka)
+    CONSTRAINT fk_sprzet_na_wyprawach_sprzet FOREIGN KEY (ID_sprzetu) REFERENCES SPRZET(ID_sprzetu)
 
 );
+
 
 
