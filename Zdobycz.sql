29daf92815c8adf4fb5c81af545dfb0051a00816
Heli0s <darthhelios44@gmail.com>
2026-01-11 10:24:42 +0100

    Modify Zdobycz table by removing UNIQUE constraint
    Removed UNIQUE constraint from the Opis column.

--- Zdobycz.sql
+++ Zdobycz.sql
@@ -7,10 +7,11 @@ CREATE TABLE Zdobycz(
     MASA FLOAT(4) NOT NULL,
     Dlugosc FLOAT(4) NOT NULL,
     Data_polowu DATE NOT NULL,
-    Opis VARCHAR2(300) UNIQUE,
+    Opis VARCHAR2(300),
     CONSTRAINT fk_zdobycz FOREIGN KEY (ID_wedkarza) REFERENCES czlonek_klubu(ID_czlonka),
     CONSTRAINT fk_zdobycz_sprzet FOREIGN KEY (ID_sprzetu) REFERENCES SPRZET(ID_sprzetu),
     CONSTRAINT fk_zdobycz_wyprawa FOREIGN KEY (ID_wyprawy) REFERENCES WYPRAWA_WEDKARSKA(ID_wyprawy)
 
 
 );
+
