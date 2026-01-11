d4e2f478954fa2288ba3d3ef25fe5fbb7dc646ca
Heli0s <darthhelios44@gmail.com>
2026-01-11 01:13:20 +0100

    Update Uczestnicy_wyprawy.sql

--- Uczestnicy_wyprawy.sql
+++ Uczestnicy_wyprawy.sql
@@ -1,8 +1,9 @@
 CREATE TABLE Uczestnicy_wyprawy (
     ID_wyprawy NUMBER(12) NOT NULL UNIQUE,
-    ID_uczestnika NUMBER(7) NOT NULL UNIQUE,
-    Nr_karty NUMBER(12) NOT NULL UNIQUE,
+    ID_uczestnika NUMBER(7) NOT NULL,
+    Nr_karty NUMBER(12) NOT NULL,
     CONSTRAINT fk_uczestnicy_wyprawy_ID_wyprawy FOREIGN KEY (ID_wyprawy) REFERENCES Wyprawa_WEDKARSKA(ID_wyprawy),
     CONSTRAINT fk_uczestnicy_czlonek_ID_czlonka FOREIGN KEY (ID_uczestnika) REFERENCES czlonek_klubu(ID_czlonka),
     CONSTRAINT fk_uczestnicy_karty_ID_karty FOREIGN KEY (Nr_karty) REFERENCES KARTA_WEDKARSKA(Nr_karty)
+
 );
