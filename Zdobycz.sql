8173fd8a7d73dee74890f4ea6618fd1dd7e2eab0
Heli0s <darthhelios44@gmail.com>
2026-01-11 01:16:21 +0100

    Fix table creation syntax in Zdobycz.sql

--- Zdobycz.sql
+++ Zdobycz.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Zdobycz(
     ID_zdobyczy NUMBER(12) PRIMARY KEY,
-    ID_wedkarza NUMBER(7) NOT NULL UNIQUE,
-    ID_sprzetu NUMBER(12) NOT NULL UNIQUE,
-    ID_wyprawy NUMBER(12) NOT NULL UNIQUE,
+    ID_wedkarza NUMBER(7) NOT NULL ,
+    ID_sprzetu NUMBER(12) NOT NULL,
+    ID_wyprawy NUMBER(12) NOT NULL,
     Gatunek VARCHAR2(100) NOT NULL,
     MASA FLOAT(4) NOT NULL,
     Dlugosc FLOAT(4) NOT NULL,
@@ -11,5 +11,6 @@ CREATE TABLE Zdobycz(
     CONSTRAINT fk_zdobycz FOREIGN KEY (ID_wedkarza) REFERENCES czlonek_klubu(ID_czlonka),
     CONSTRAINT fk_zdobycz_sprzet FOREIGN KEY (ID_sprzetu) REFERENCES SPRZET(ID_sprzetu),
     CONSTRAINT fk_zdobycz_wyprawa FOREIGN KEY (ID_wyprawy) REFERENCES WYPRAWA_WEDKARSKA(ID_wyprawy)
+
 
 );
