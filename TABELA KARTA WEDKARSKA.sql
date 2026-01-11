7f938933f014a6f121dff244e6a03e6a2ee5a019
Heli0s <darthhelios44@gmail.com>
2026-01-11 01:18:20 +0100

    Remove UNIQUE constraint from ID_wlasciciela

--- TABELA KARTA WEDKARSKA.sql
+++ TABELA KARTA WEDKARSKA.sql
@@ -1,9 +1,10 @@
 CREATE TABLE karta_wedkarska(
     Nr_karty NUMBER(12) PRIMARY KEY,
-    ID_wlasciciela NUMBER(7) NOT NULL UNIQUE,
+    ID_wlasciciela NUMBER(7) NOT NULL,
     Typ VARCHAR2(15) NOT NULL,
     DATA_WAZNOSCI DATE NOT NULL,
     CONSTRAINT fk_czlonek_karta_ID_wlasciciela 
     FOREIGN KEY(ID_wlasciciela)
     REFERENCES czlonek_klubu(ID_czlonka)
 );
+
