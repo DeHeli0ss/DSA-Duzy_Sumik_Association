4286f8abb637abc85d725cb76e2c52db33163a1c
Heli0s <darthhelios44@gmail.com>
2026-01-11 10:52:32 +0100

    Update TABELA KONTO SYSTEMOWE.sql

--- TABELA KONTO SYSTEMOWE.sql
+++ TABELA KONTO SYSTEMOWE.sql
@@ -1,19 +1,4 @@
-------------------------------------------------------
---  Tworzenie tabeli Konto Systemowe gdzie można    --
---  logować się za pomocą nr_teleonu lub emaila     --
-------------------------------------------------------
-CREATE TABLE KONTO_SYSTEMOWE(
-        id_czlonka NUMBER(7) PRIMARY KEY,
-        nr_telefonu NUMBER(9) NULL UNIQUE,
-        email VARCHAR2(80) NULL UNIQUE,
-        haslo VARCHAR2(20) NOT NULL,
-------------------------------------------------------------------------
---  Sprawdzenie czy do logowania spełniony jest warunek minimum czyli --
---  czyli podanie nr_teleonu lub emaila                               --
-------------------------------------------------------------------------    
-        CONSTRAINT check_logowanie CHECK
-        (email is NOT NULL or nr_telefonu is NOT NULL)
-        
-);
+
+
 
 
